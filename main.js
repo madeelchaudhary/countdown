81af4f2a8e7b305703f658884c9c992b19437b99
Muhammad Adeel Chaudhary <madeelchaudhry@gmail.com>
2021-04-25 21:47:55 +0500

    Update main.js
    Automatically Update Months After Reaching Zero

--- main.js
+++ main.js
@@ -7,24 +7,31 @@ function countdown() {
     let nowTime = now.getTime();
     let gap = countTime - nowTime;
 
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
-
-    let d = Math.floor(gap / (day));
-    let h = Math.floor((gap % (day)) / (hour));
-    let m = Math.floor((gap % (hour)) / (minute));
-    let s = Math.floor((gap % (minute)) / (second));
-
-    document.querySelector('.day').innerHTML = d
-    document.querySelector('.hour').innerText = h
-    document.querySelector('.min').innerText = m
-    document.querySelector('.sec').innerText = s
-
-    setTimeout(() => {
+    if (gap <= 0) {
+        let nowMonth = countDate.getMonth();
+        countDate.setMonth(nowMonth + 1);
         countdown();
-    }, 1000);
+    }
+    else {
+        let second = 1000;
+        let minute = second * 60;
+        let hour = minute * 60;
+        let day = hour * 24;
+
+        let d = Math.floor(gap / (day));
+        let h = Math.floor((gap % (day)) / (hour));
+        let m = Math.floor((gap % (hour)) / (minute));
+        let s = Math.floor((gap % (minute)) / (second));
+
+        document.querySelector('.day').innerHTML = d
+        document.querySelector('.hour').innerText = h
+        document.querySelector('.min').innerText = m
+        document.querySelector('.sec').innerText = s
+
+        setTimeout(() => {
+            countdown();
+        }, 1000);
+    }
 }
 
 countdown()
